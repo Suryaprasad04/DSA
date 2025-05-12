272ffbb567a2c18bced07513a84fcdb12e21db6f
Suryaprasad04 <150020783+Suryaprasad04@users.noreply.github.com>
2025-05-13 01:04:19 +0530

    Create MinimumNumberOfDeletionsAndInsertions.java

---
+++ Dp/MinimumNumberOfDeletionsAndInsertions.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minOperations(String s1, String s2) {
+        // Your code goes here
+        int deletion=s1.length()-lcs(s1,s2);
+        int insertion=s2.length()-lcs(s1,s2);
+        return deletion+insertion;
+    }
+    static int lcs(String s1, String s2) {
+        // code here
+        int m=s1.length(),n=s2.length();
+        int [][]t=new int[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                t[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    t[i][j]=1+t[i-1][j-1];
+                }else{
+                    t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+    return t[m][n];
+    }
+}

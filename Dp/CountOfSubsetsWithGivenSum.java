0794376e0639cd2d4996cf45ef9bddf49edc38a7
Suryaprasad04 <150020783+Suryaprasad04@users.noreply.github.com>
2025-05-14 21:27:31 +0530

    Create CountOfSubsetsWithGivenSum.java

---
+++ Dp/CountOfSubsetsWithGivenSum.java
@@ -0,0 +1,36 @@
+class Solution {
+    // Function to calculate the number of subsets with a given sum   class Solution {
+    public int perfectSum(int[] nums, int target) {
+        int n = nums.length;
+        int[][] t = new int[n + 1][target + 1];
+        
+
+        // Base condition using your style
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (i == 0 && j == 0) {
+                    t[i][j] = 1;
+                } else if (i == 0) {
+                    t[i][j] = 0;
+                } else if (j == 0) {
+                    t[i][j] = 1;
+                }
+            }
+        }
+
+        // Fill the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {//see here it is starting from 0
+                if (nums[i - 1] <= j) {
+                    t[i][j] = (t[i - 1][j - nums[i - 1]] + t[i - 1][j]);
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+
+        return t[n][target];
+    }
+
+
+}
